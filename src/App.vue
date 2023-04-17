23fe0b01c120cc2fa08cc2466f6d9d8f1404b701
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2023-04-16 20:43:25 -0600

    Fundamentos de vue con cli

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <counter-view title="entraron" :start="10"></counter-view>
+  <counter-view title="salieron" :start="15"></counter-view>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import CounterView from "./components/CounterView.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    CounterView,
   },
 };
 </script>
---
+++ src/components/CounterView.vue
@@ -0,0 +1,73 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p>
+    {{ counter }} <sup>2={{ squareCounter }}</sup>
+  </p>
+  <!-- <p>
+    {{ counter }} <sup>2={{ squareCounter }}</sup>
+  </p> -->
+  <div>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter-view",
+  props: {
+    title: {
+      type: String,
+    },
+    start: {
+      type: Number,
+      required: true,
+      default: 10,
+      validator(value) {
+        return value > 0 && value < 21;
+      },
+    },
+  },
+  data() {
+    return {
+      counter: this.start,
+    };
+  },
+  methods: {
+    getSquare() {
+      return this.counter * this.counter;
+    },
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+  },
+  computed: {
+    squareCounter() {
+      console.log("square counter");
+      return this.counter * this.counter;
+    },
+    customTitle() {
+      return this.title || "counter";
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  margin: 0 5px;
+  background-color: #64bb87;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
+</style>
